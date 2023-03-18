f2cd41f6d4fccc953a222a2529eeb2cfcde97fa1
Pablo Agüera Hernández <98048002+pablo-2k03@users.noreply.github.com>
2023-03-18 09:41:45 +0100

    Update lomoFinalizado.c

--- lomoFinalizado.c
+++ lomoFinalizado.c
@@ -41,10 +41,17 @@ int main(int argc,char * argv[]){
         fprintf(stderr,"Other Usage: lomo retardo nTrenes\n");
         return 1;
     }
-    if(!strcmp(argv[1],"--mapa")){
-        LOMO_generar_mapa("i0959394","i0919297");
+    if(argc == 2 && !strcmp(argv[1],"--mapa")){
+        if(LOMO_generar_mapa("i0959394","i0919297") == -1){
+            fprintf(stderr,"Error al generar el mapa.\n");
+            return 1;
+        }
+        else{
+            fprintf(stderr,"Mapa generado correctamente.\n");
+            return 1;
+        }
     }
-    if(argc == 3){
+    if(comprobarPrimerArgumento(argv[1])!=-1 && comprobarSegundoArgumento(argv[2])!=-1){
         int retardo=0;
         recursosIPCS.nTrenes=0;
         tipoMensaje tipoMensajes;
@@ -91,16 +98,12 @@ int main(int argc,char * argv[]){
 
        
        
-        LOMO_inicio(retardo,recursosIPCS.idSemaforo,recursosIPCS.idBuzon,"i0959394","i0919297");
-        //Creacion del tipo de mensaje.
-        int i;
-        struct mensaje msg;
-        msg.tipo = TIPO_TRENNUEVO;
-        msg.tren = 0;
-        msg.x = 0;
-        msg.y = 0;
-        int x,y;
-
+        if(LOMO_inicio(retardo,recursosIPCS.idSemaforo,recursosIPCS.idBuzon,"i0959394","i0919297") == -1){
+            fprintf(stderr,"Error al iniciar LOCOMOTION.\n");
+            return 1;
+        }
+        
+        int x;
         //Mandar mensajes de tipo x a todas las casillas.
         for(x = 1; x <= 75*17; x++){ 
             tipoMensajes.tipo = x; //Casilla a la q va.
@@ -112,6 +115,14 @@ int main(int argc,char * argv[]){
             fprintf(stderr,"Trenes: %d",nTrenes);
         #endif
         
+        //Creacion del tipo de mensaje.
+        int i;
+        struct mensaje msg;
+        msg.tipo = TIPO_TRENNUEVO;
+        msg.tren = 0;
+        msg.x = 0;
+        msg.y = 0;
+        
         
         for(i = 0; i< recursosIPCS.nTrenes; i++){
             if(fork()==0){
@@ -120,33 +131,53 @@ int main(int argc,char * argv[]){
                     fprintf(stderr,"sigaction ERROR.\n");
                 }
                 //Crear un tren.
-                msgsnd(recursosIPCS.idBuzon,&msg,sizeof(struct mensaje )- sizeof(long),0);
-                msgrcv(recursosIPCS.idBuzon,&msg,sizeof(struct mensaje)- sizeof(long),TIPO_RESPTRENNUEVO,0);
+                if(msgsnd(recursosIPCS.idBuzon,&msg,sizeof(struct mensaje )- sizeof(long),0) == -1){
+                    fprintf(stderr,"Error al enviar el mensaje de creación del tren.\n");
+                }
+                if(msgrcv(recursosIPCS.idBuzon,&msg,sizeof(struct mensaje)- sizeof(long),TIPO_RESPTRENNUEVO,0) == -1){
+                    fprintf(stderr,"Error al recibir la respuesta de la biblioteca en la creación del tren.\n");
+                }
                 
                 while(1){             
                     msg.tipo =  TIPO_PETAVANCE;
                     int posAnterior = msg.y;           
-                    msgsnd(recursosIPCS.idBuzon,&msg,sizeof(struct mensaje) - sizeof(long),0);
-                    msgrcv(recursosIPCS.idBuzon,&msg,sizeof(struct mensaje)- sizeof(long),TIPO_RESPPETAVANCETREN0+msg.tren,0);
+                    if(msgsnd(recursosIPCS.idBuzon,&msg,sizeof(struct mensaje) - sizeof(long),0) == -1){
+                        fprintf(stderr,"Error al enviar el mensaje de petición de avance del tren.\n");
+                    }
+                    if(msgrcv(recursosIPCS.idBuzon,&msg,sizeof(struct mensaje)- sizeof(long),TIPO_RESPPETAVANCETREN0+msg.tren,0) == -1){
+                        fprintf(stderr,"Error al recibir la respuesta de la biblioteca en la petición de avance del tren.\n");
+                    }
                     
                     //Si hay un mensaje disponible en el buzon de mensajes, se lee y continua y sino, se queda esperando uno.
-                    
-                    msgrcv(recursosIPCS.idBuzonMsj,&tipoMensajes,sizeof(tipoMensajes) - sizeof(long),msg.y* 75 + msg.x+1,0); // +1 en caso de coord x = 0 y coord y = 0
+                    // +1 en caso de coord x = 0 y coord y = 0
+                    if(msgrcv(recursosIPCS.idBuzonMsj,&tipoMensajes,sizeof(tipoMensajes) - sizeof(long),msg.y* 75 + msg.x+1,0) == -1){
+                        fprintf(stderr,"Error al recibir el mensaje de la casilla.\n");
+                    } 
 
-                    msg.tipo = TIPO_AVANCE;
                     //Mensajes para realizar el avance del tren.
-                    msgsnd(recursosIPCS.idBuzon,&msg,sizeof(struct mensaje) - sizeof(long),0);
-                    msgrcv(recursosIPCS.idBuzon,&msg,sizeof(struct mensaje)- sizeof(long), TIPO_RESPAVANCETREN0+msg.tren,0);
+                    msg.tipo = TIPO_AVANCE;
+                    if(msgsnd(recursosIPCS.idBuzon,&msg,sizeof(struct mensaje) - sizeof(long),0) == -1){
+                        fprintf(stderr,"Error al enviar el mensaje de avance del tren.\n");
+                    }
+                    if(msgrcv(recursosIPCS.idBuzon,&msg,sizeof(struct mensaje)- sizeof(long), TIPO_RESPAVANCETREN0+msg.tren,0) == -1){
+                        fprintf(stderr,"Error al recibir la respuesta de la biblioteca en el avance del tren.\n");
+                    }
+                    //Sin if porque es de tipo void.
                     LOMO_espera(posAnterior,msg.y);
                     //Libera el culo del tren, porque en msg.x y msg.y esta la posicion anterior.
                     if(msg.x >= 0 && msg.y  >=0){
                         tipoMensajes.tipo = msg.y* 75 + msg.x+1; // liberar la casilla que calculas.
-                        msgsnd(recursosIPCS.idBuzonMsj,&tipoMensajes,sizeof(tipoMensajes)- sizeof(long),0);                         
+                        if(msgsnd(recursosIPCS.idBuzonMsj,&tipoMensajes,sizeof(tipoMensajes)- sizeof(long),0) == -1){
+                            fprintf(stderr,"Error al enviar el mensaje de liberación de la casilla.\n");
+                        }                         
                     }     
                 }
             }
         }
         pause();
+    }
+    else{
+        fprintf(stderr,"Introduzca unos argumentos validos.\n");
     } 
     return 0;
 }
@@ -154,7 +185,9 @@ int main(int argc,char * argv[]){
 
 void signalHandler(int n){
     //fprintf(stderr,"Entro a la manejadora.\n");    
-    LOMO_fin();
+    if(LOMO_fin() == -1){
+        fprintf(stderr,"Error al finalizar LOCOMOTION.\n");
+    }
     int i;
     for(i=0; i< recursosIPCS.nTrenes;i++){
         wait(0);
@@ -174,7 +207,7 @@ void signalHandler(int n){
 
 
 int comprobarPrimerArgumento(char *argv){
-    if(atoi(argv) < 0)
+    if(atoi(argv) < 0 || comprobarSegundoArgumento(argv) == -1)
         return -1;
    
     return atoi(argv);
